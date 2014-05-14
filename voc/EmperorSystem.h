a6f01650e45677b63109023ea297344d76097cd1
fizzafruit <soardex@vastorigins.net>
2014-05-14 10:34:47 +0800

    -- Added proper licensing.

--- voc/EmperorSystem.h
+++ voc/EmperorSystem.h
@@ -12,7 +12,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) IOIO, 2014
+ * Copyright (C) Fitz Abucay, 2014
  */
 
 #ifndef EMPERORSYSTEM_H
